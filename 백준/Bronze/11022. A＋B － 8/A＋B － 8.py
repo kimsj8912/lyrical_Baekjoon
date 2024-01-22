0eae658b16854d09037c1025345cedb182e702c2
kimseojeong <kimsj8912@kookmin.ac.kr>
2024-01-22 22:41:03 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+import sys
+testCases = int(input())
+num = 1
+for i in range(testCases):
+    a, b = map(int, sys.stdin.readline().split())
+    print(f"Case #{num}: {a} + {b} = {a+b}")
+    num += 1
